a812025c78dd7e094586d268ccd1fa896e376c04
2000037023 <nathelmundackal@gmail.com>
2020-11-27 12:12:53 -0500

    synchronized ball

--- sketch.js
+++ sketch.js
@@ -1,29 +1,49 @@
-var ball;
+var ball, ball_position;
+var database;
+var ball_position;
 
 function setup(){
+    database=firebase.database();
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    ball_position=database.ref("ball/position");
+    ball_position.on("value",readposition,showError);
 }
 
 function draw(){
     background("white");
+    if(ball_position!==undefined){
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
+        
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
+}
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+    database.ref("ball/position").set({
+        'x':ball_position.x+ x,
+        'y':ball_position.y+ y
+    }); 
+}
+
+function readposition(data){
+ball_position=data.val();
+console.log(ball_position);
+ball.x=ball_position.x;
+ball.y=ball_position.y;
+}
+function showError(){
+console.log('error in writing the database');
 }
